8fa129ca6aabbf02b861f2987818948002ce87ba
jiangstack <jiangstack@163.com>
2026-05-09 22:39:29 +0800

    键盘顶起优化

--- app/src/main/java/org/jiangstack/mytavern/MainActivity.kt
+++ app/src/main/java/org/jiangstack/mytavern/MainActivity.kt
@@ -3,6 +3,7 @@ package org.jiangstack.mytavern
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
@@ -32,6 +33,7 @@ import org.jiangstack.mytavern.ui.theme.MyTavernTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         val container = (application as MyTavernApplication).container
         val userPreferencesRepository = container.userPreferencesRepository
 
